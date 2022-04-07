de136f2af87bc333520c5112d9f81ff3de6b3ec8
Ouraa <99221379+Oura-Ubot@users.noreply.github.com>
2022-04-08 01:20:29 +0700

    Update gabutan.py

--- userbot/modules/gabutan.py
+++ userbot/modules/gabutan.py
@@ -8,132 +8,132 @@ from userbot import CMD_HELP
 from userbot.utils import edit_or_reply, poci_cmd
 
 
-@poci_cmd(pattern="aa(?: |$)(.*)")
+@poci_cmd(pattern="gelo(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**ANJAY ADA GIPEAWAY NIH**")
+    await edit_or_reply(event, "**`𝙐𝙪𝙪..𝙀𝘿𝘼𝙉...𝙍𝘼𝘿𝘼 𝙂𝙀𝙇𝙇𝙊𝙊𝙊...😜**")
 
 
-@poci_cmd(pattern="bb(?: |$)(.*)")
+@poci_cmd(pattern="ggg(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**BABI SOALNYA GA DANTA BET**")
 
 
-@poci_cmd(pattern="cc(?: |$)(.*)")
+@poci_cmd(pattern="ccc(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**ANJING LAMA BET, TURUNIN SOALNYA**")
 
 
-@poci_cmd(pattern="dd(?: |$)(.*)")
+@poci_cmd(pattern="ddd(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MANA SOALNYA BUJED, GUA NUNGGUIN DARI TADI**")
 
 
-@poci_cmd(pattern="ee(?: |$)(.*)")
+@poci_cmd(pattern="eee(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MINIMAL KALO HADIAHNYA DIKIT, SOALNYA DANTA DIKIT YA ANJENG 😁!!**")
 
 
-@poci_cmd(pattern="ff(?: |$)(.*)")
+@poci_cmd(pattern="fff(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**HETT GIPEAWAY NYA DIKIT BET, DASAR GC MISKIN CUIHHH!!**")
 
 
-@poci_cmd(pattern="gg(?: |$)(.*)")
+@poci_cmd(pattern="ggg(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**KALO GIPEAWAY SOALNYA YANG BENER YA NGENTOTTT**")
 
 
-@poci_cmd(pattern="hh(?: |$)(.*)")
+@poci_cmd(pattern="hhh(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MINGGIR-MINGGIR DONATUR MAU LEWAT**")
 
 
-@poci_cmd(pattern="ii(?: |$)(.*)")
+@poci_cmd(pattern="iui(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**MISKIN MISKIN AJA TOD, GAUSAH SOK SOK AN JADI DONATUR!!**")
 
 
-@poci_cmd(pattern="jj(?: |$)(.*)")
+@poci_cmd(pattern="jjj(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GCAST DULU AHHHHH, KALI AJA MENANG GIPEAWAY!!**")
 
 
-@poci_cmd(pattern="kk(?: |$)(.*)")
+@poci_cmd(pattern="kkk(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**JAWABAN YANG BENER YANG MANA ANJENG**")
 
 
-@poci_cmd(pattern="ll(?: |$)(.*)")
+@poci_cmd(pattern="lkl(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**PAANSI SOALNYA PRIK BANGET!!**")
 
 
-@poci_cmd(pattern="mm(?: |$)(.*)")
+@poci_cmd(pattern="kmm(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**NAJISSS GIPEAWAY NYA CUMAN CEBAN YANG BANYAKAN NAPAH!!**")
 
 
-@poci_cmd(pattern="nn(?: |$)(.*)")
+@poci_cmd(pattern="knn(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**OMEGAT GUA MENANG GIPEAWAY, MAYAN BUAT PICIES!!** 🥵🥵")
 
 
-@poci_cmd(pattern="oo(?: |$)(.*)")
+@poci_cmd(pattern="koo(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**RAME JUGA DISINI, IKUTAN AHHH!!**")
 
 
-@poci_cmd(pattern="pp(?: |$)(.*)")
+@poci_cmd(pattern="jpp(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GC ANJING SOALNYA GA DANTA SEMUA DAHLAH!!**")
 
 
-@poci_cmd(pattern="qq(?: |$)(.*)")
+@poci_cmd(pattern="qjq(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**HETTT SOALNYA GAMPANG BET, BARI MEREM GE BISA INI MAH** 🥱")
 
 
-@poci_cmd(pattern="rr(?: |$)(.*)")
+@poci_cmd(pattern="jrr(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**UDAH UDAH GIPEAWAY NYA UDAHAN, YAH KASIAN BET KETINGGALAN**")
 
 
-@poci_cmd(pattern="ss(?: |$)(.*)")
+@poci_cmd(pattern="jss(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**CAPE CAPE GIKES SOALNYA GA DANTA KONTOLL**")
 
 
-@poci_cmd(pattern="tt(?: |$)(.*)")
+@poci_cmd(pattern="jtt(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**CLUE NYA APA ANJENG, SOALNYA GAJELAS**")
 
     
-@poci_cmd(pattern="uu(?: |$)(.*)")
+@poci_cmd(pattern="juu(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**DIMANA ADA GIPEAWAY DISITU ADA GUA WHAHAHAHAHA**")
     
    
-@poci_cmd(pattern="vv(?: |$)(.*)")
+@poci_cmd(pattern="vjv(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
 
 
-@poci_cmd(pattern="ww(?: |$)(.*)")
+@poci_cmd(pattern="jww(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
 
 
-@poci_cmd(pattern="xx(?: |$)(.*)")
+@poci_cmd(pattern="jxx(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
 
 
-@poci_cmd(pattern="yy(?: |$)(.*)")
+@poci_cmd(pattern="yoy(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
 
 
-@poci_cmd(pattern="zz(?: |$)(.*)")
+@poci_cmd(pattern="jzz(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
     
