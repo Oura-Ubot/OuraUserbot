7fd5c106222145e0c32e72b5893a82a215c09eac
Ouraa <99221379+Oura-Ubot@users.noreply.github.com>
2022-04-08 00:46:54 +0700

    Update giveaway.py

--- userbot/modules/giveaway.py
+++ userbot/modules/giveaway.py
@@ -10,132 +10,132 @@ from userbot.utils import edit_or_reply, poci_cmd
 
 @poci_cmd(pattern="aa(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**ANJAY ADA GIPEAWAY NIH**")
+    await edit_or_reply(event, "**`𝙒𝘼𝙃𝙆𝙆𝙆... 𝘼𝘿𝘼 𝙂𝙄𝙋𝙀𝙒𝙀 𝙉𝙄𝘾𝙃...`**")
 
 
 @poci_cmd(pattern="bb(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**BABI SOALNYA GA DANTA BET**")
+    await edit_or_reply(event, "**`𝙋𝘼𝘼𝙉𝙎𝙄... 𝙋𝙍𝙄𝙆𝙆 𝘽𝘼𝙉𝙂𝙀𝙏 𝙎𝙊𝘼𝙇𝙉𝙔𝘼 𝙉𝙂𝙀𝙉𝙏𝙊𝙏𝙏𝙏...`**")
 
 
 @poci_cmd(pattern="cc(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**ANJING LAMA BET, TURUNIN SOALNYA**")
+    await edit_or_reply(event, "**`𝘽𝙄𝙇𝘼𝙉𝙂𝙉𝙔𝘼 𝙐𝙇𝘼𝙉𝙂...𝙆𝙊𝙆 𝙈𝘼𝙉𝘿𝘼𝙉𝙂 𝙇𝙄𝙎𝙏𝙀𝙉𝙀𝙍 𝙎𝙄𝙃 𝙆𝙊𝙉𝙏𝙊𝙇𝙇𝙇..`**")
 
 
 @poci_cmd(pattern="dd(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**MANA SOALNYA BUJED, GUA NUNGGUIN DARI TADI**")
+    await edit_or_reply(event, "**`𝙔𝘼𝙉𝙂 𝙉𝙂𝘼𝙎𝙄𝙃 𝙎𝙊𝘼𝙇 𝘼𝙉𝘼𝙆 𝙃𝘼𝙍𝘼𝙈 𝙄𝙉𝙄 𝙈𝘼𝙃 𝘽𝙇𝙊𝙐𝙂𝙃...`**")
 
 
 @poci_cmd(pattern="ee(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**MINIMAL KALO HADIAHNYA DIKIT, SOALNYA DANTA DIKIT YA ANJENG 😁!!**")
+    await edit_or_reply(event, "**`𝙆𝘼𝙇𝙊 𝙃𝘼𝘿𝙄𝘼𝙃 𝙉𝙔𝘼 𝘿𝙄𝙆𝙄𝙏, 𝙎𝙊𝘼𝙇𝙉𝙔𝘼 𝙂𝘼𝙐𝙎𝘼𝙃 𝙋𝙍𝙄𝙆𝙆 𝙉𝙂𝙀𝙉𝙊𝙏𝙏𝙏..`**")
 
 
 @poci_cmd(pattern="ff(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**HETT GIPEAWAY NYA DIKIT BET, DASAR GC MISKIN CUIHHH!!**")
+    await edit_or_reply(event, "**`𝙏𝙀𝙏𝘼𝙋 𝙂𝙄𝙆𝙀𝙎, 𝙒𝘼𝙇𝘼𝙐𝙋𝙐𝙉 𝙂𝘼 𝙋𝙀𝙍𝙉𝘼𝙃 𝘿𝘼𝙋𝙀𝙏 𝙂𝙄𝙋𝙀𝙒𝙀...🐣`**")
 
 
 @poci_cmd(pattern="gg(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**KALO GIPEAWAY SOALNYA YANG BENER YA NGENTOTTT**")
+    await edit_or_reply(event, "**`𝙎𝙊𝘼𝙇 𝙋𝙍𝙄𝙆𝙆=𝙂𝘽𝘼𝙉 𝙍𝘼𝙈𝙀-𝙍𝘼𝙈𝙀...`**")
 
 
 @poci_cmd(pattern="hh(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**MINGGIR-MINGGIR DONATUR MAU LEWAT**")
+    await edit_or_reply(event, "**`𝘿𝙊𝙉𝘼𝙏𝙐𝙍 𝙋𝘼𝙇𝙆𝙊𝙍 𝙎𝙐𝙈𝙋𝘼𝙃𝙄𝙉 𝙏𝙄𝙏𝙄𝙏 𝙉𝙔𝘼 𝘽𝙀𝙉𝙂𝙆𝘼𝙆 𝙎𝙀𝙂𝙀𝘿𝙀 𝘽𝙊𝙏𝙊𝙇 𝘼𝙌𝙐𝘼...`**")
 
 
 @poci_cmd(pattern="ii(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**MISKIN MISKIN AJA TOD, GAUSAH SOK SOK AN JADI DONATUR!!**")
+    await edit_or_reply(event, "**`𝘿𝙊𝙈𝙋𝙀𝙏 𝙆𝙊𝙎𝙊𝙉𝙂 𝘼𝙅𝘼 𝙎𝙊𝙆 𝙎𝙊𝘼𝙉 𝙅𝘼𝘿𝙄 𝘿𝙊𝙉𝘼𝙏𝙐𝙍 𝙇𝙐 𝙆𝙊𝙉𝙏𝙊𝙇𝙇𝙇`**")
 
 
 @poci_cmd(pattern="jj(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**GCAST DULU AHHHHH, KALI AJA MENANG GIPEAWAY!!**")
+    await edit_or_reply(event, "**`𝙂𝘾𝘼𝙎𝙏 𝘽𝙇𝙊𝙐𝙂𝙃...𝙅𝘼𝙉𝙂𝘼𝙉 𝙈𝘼𝙐 𝙂𝙄𝙋𝙀𝙒𝙀 𝙉𝙔𝘼 𝘿𝙊𝘼𝙉𝙂 𝙉𝙔𝙀𝙏𝙏...`**")
 
 
 @poci_cmd(pattern="kk(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**JAWABAN YANG BENER YANG MANA ANJENG**")
+    await edit_or_reply(event, "**`𝙔𝘼𝙉𝙂 𝘿𝘼𝙋𝙀𝙏 𝙂𝙄𝙋𝙀𝙒𝙀 𝙈𝙄𝙉𝙄𝙈𝘼𝙇 𝘽𝘼𝙂𝙄 𝙇𝘼𝙃 𝙉𝙂𝙀𝙉𝙏𝙊𝙏𝙏𝙏...`**")
 
 
 @poci_cmd(pattern="ll(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**PAANSI SOALNYA PRIK BANGET!!**")
+    await edit_or_reply(event, "**`𝘽𝙄𝙎𝙈𝙄𝙇𝙇𝘼𝙃 𝘿𝘼𝙋𝙀𝙏 𝙂𝙄𝙋𝙀𝙒𝙀 🐣`**")
 
 
 @poci_cmd(pattern="mm(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**NAJISSS GIPEAWAY NYA CUMAN CEBAN YANG BANYAKAN NAPAH!!**")
+    await edit_or_reply(event, "**`𝙂𝙄𝙋𝙀𝙒𝙀 𝙉𝙔𝘼 𝙂𝘼𝙅𝙀𝙇𝘼𝙎 𝙆𝙊𝙉𝙏𝙊𝙇𝙇𝙇, 𝘾𝙐𝙄𝙃𝙃...`**")
 
 
 @poci_cmd(pattern="nn(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**OMEGAT GUA MENANG GIPEAWAY, MAYAN BUAT PICIES!!** 🥵🥵")
+    await edit_or_reply(event, "**`𝘽𝙄𝙎𝙈𝙄𝙇𝙇𝘼𝙃 𝘿𝘼𝙋𝙀𝙏 𝙂𝙄𝙋𝙀𝙒𝙀 𝙇𝘼𝙉𝙂𝙎𝙐𝙉𝙂 𝙊𝙍𝘿𝙀𝙍 𝙋𝙄𝘾𝙄𝙀𝙎...`** 🥵🥵")
 
 
 @poci_cmd(pattern="oo(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**RAME JUGA DISINI, IKUTAN AHHH!!**")
+    await edit_or_reply(event, "**`𝙈𝙄𝙎𝙄 𝘼𝙉𝙅𝙄𝙉𝙂...𝘿𝙊𝙉𝘼𝙏𝙐𝙍 𝙈𝘼𝙐 𝙇𝙀𝙒𝘼𝙏 😎`**")
 
 
 @poci_cmd(pattern="pp(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**GC ANJING SOALNYA GA DANTA SEMUA DAHLAH!!**")
+    await edit_or_reply(event, "**`𝘾𝙐𝙄𝙃...𝙂𝙄𝙋𝙀𝙒𝙀 𝙉𝙔𝘼 𝙆𝙀𝘽𝘼𝙉𝙔𝘼𝙆𝘼𝙉 𝘿𝙍𝘼𝙈𝘼 𝙆𝙀𝙆 𝙄𝙉𝘿𝙊𝙎𝙄𝘼𝙍`**")
 
 
 @poci_cmd(pattern="qq(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**HETTT SOALNYA GAMPANG BET, BARI MEREM GE BISA INI MAH** 🥱")
+    await edit_or_reply(event, "**`𝙎𝙊𝘼𝙇𝙉𝙔𝘼 𝙎𝙐𝙎𝘼𝙃 𝘿𝙄𝙆𝙄𝙏 𝙉𝙂𝘼𝙋𝘼 𝙉𝙔𝙀𝙏𝙏...`** 🥱")
 
 
 @poci_cmd(pattern="rr(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**UDAH UDAH GIPEAWAY NYA UDAHAN, YAH KASIAN BET KETINGGALAN**")
+    await edit_or_reply(event, "**`𝙏𝙐𝙍𝙐𝙉𝙄𝙉 𝙂𝙄𝙋𝙀𝙒𝙀 𝘼𝙏𝙊 𝙂𝙐𝘼 𝙇𝘼𝙋𝙊𝙍𝙄𝙉 𝙆𝙀 𝙈𝘼𝙇𝘼𝙄𝙆𝘼𝙏 𝙄𝙕𝙍𝘼𝙄𝙇 𝙉𝙄𝙃???`**")
 
 
 @poci_cmd(pattern="ss(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**CAPE CAPE GIKES SOALNYA GA DANTA KONTOLL**")
+    await edit_or_reply(event, "**`𝙈𝘼𝙆𝘼𝙉𝙂𝘼 𝙂𝘾𝘼𝙎𝙏 𝙆𝙊𝙉𝙏𝙊𝙇𝙇𝙇...`**")
 
 
-@poci_cmd(pattern="tt(?: |$)(.*)")
+@poci_cmd(pattern="clue(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**CLUE NYA APA ANJENG, SOALNYA GAJELAS**")
+    await edit_or_reply(event, "**`𝘾𝙇𝙐𝙀𝙀 𝙉𝙂𝙀𝙉𝙏𝙊𝙏𝙏𝙏...𝙂𝙐𝘼 𝘽𝙐𝙆𝘼𝙉 𝘿𝙐𝙆𝙐𝙉`**")
 
     
 @poci_cmd(pattern="uu(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**DIMANA ADA GIPEAWAY DISITU ADA GUA WHAHAHAHAHA**")
+    await edit_or_reply(event, "**`𝙂𝙄𝙋𝙀𝙒𝙀 𝙉𝙔𝘼 𝘿𝘼𝙃 𝘿𝘼𝙋𝙀𝙏 𝙉𝙄𝘾𝙃...𝙏𝙄𝙉𝙂𝙂𝘼𝙇 𝘼𝙔𝘼𝙉𝙂 𝙉𝙔𝘼 :(`**")
     
    
 @poci_cmd(pattern="vv(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
+    await edit_or_reply(event, "**`𝙇𝘼𝙈𝘼 𝘽𝘼𝙉𝙂𝙀𝙏𝙏 𝙎𝙄𝙃 𝙉𝙂𝙀𝙉𝙏𝙊𝙏𝙏𝙏...`**")
 
 
 @poci_cmd(pattern="ww(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
+    await edit_or_reply(event, "**`𝙂𝙄𝙋𝙀𝙒𝙀 𝘿𝙐𝙇𝙐...𝘽𝘼𝙍𝙐 𝙆𝘼𝙈𝙐...🐣`**")
 
 
 @poci_cmd(pattern="xx(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
+    await edit_or_reply(event, "**`𝙂𝘼𝙍𝘼-𝙂𝘼𝙍𝘼 𝙂𝘼 𝘿𝘼𝙋𝙀𝙏 𝙂𝙄𝙋𝙀𝙒𝙀 𝙅𝘼𝘿𝙄 𝘿𝙀𝙋𝙍𝙀𝙎𝙄 𝙋𝙀𝙉 𝙋𝙐𝙉𝙔𝘼 𝘼𝙔𝘼𝙉𝙂 :(`**")
 
 
 @poci_cmd(pattern="yy(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
+    await edit_or_reply(event, "**`𝘾𝙐𝙄𝙃𝙃𝙃... 𝙋𝘼𝙇𝘼𝙇𝙊 𝙂𝙀𝘿𝙀 𝙆𝙊𝙉𝙏𝙊𝙇𝙇𝙇, 𝙂𝘼𝙅𝙀𝙇𝘼𝙎 𝘽𝘼𝙏...`**")
 
 
 @poci_cmd(pattern="zz(?: |$)(.*)")
 async def _(event):
-    await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
+    await edit_or_reply(event, "**`𝘽𝘼𝘽𝘼𝙔... 𝙂𝙐𝘼 𝙐𝘿𝘼𝙃 𝘿𝘼𝙋𝙀𝙏 𝙂𝙄𝙋𝙀𝙒𝙀 𝙉𝙔𝘼, 𝙒𝙇𝙀𝙀𝙀...🤪`**")
     
 
 CMD_HELP.update(
