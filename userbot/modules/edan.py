c2ee2a4adc180794e5190c9b47dcc72e66345cbe
Ouraa <99221379+Oura-Ubot@users.noreply.github.com>
2022-04-08 00:16:59 +0700

    Create edan.py

---
+++ userbot/modules/edan.py
@@ -0,0 +1,147 @@
+# @Owaitingforyou
+# Oura-Userbot
+# Recode by @OuraCakep
+
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import edit_or_reply, poci_cmd
+
+
+@poci_cmd(pattern="aa(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**ANJAY ADA GIPEAWAY NIH**")
+
+
+@poci_cmd(pattern="bb(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**BABI SOALNYA GA DANTA BET**")
+
+
+@poci_cmd(pattern="cc(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**ANJING LAMA BET, TURUNIN SOALNYA**")
+
+
+@poci_cmd(pattern="dd(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**MANA SOALNYA BUJED, GUA NUNGGUIN DARI TADI**")
+
+
+@poci_cmd(pattern="ee(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**MINIMAL KALO HADIAHNYA DIKIT, SOALNYA DANTA DIKIT YA ANJENG 😁!!**")
+
+
+@poci_cmd(pattern="ff(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**HETT GIPEAWAY NYA DIKIT BET, DASAR GC MISKIN CUIHHH!!**")
+
+
+@poci_cmd(pattern="gg(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**KALO GIPEAWAY SOALNYA YANG BENER YA NGENTOTTT**")
+
+
+@poci_cmd(pattern="hh(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**MINGGIR-MINGGIR DONATUR MAU LEWAT**")
+
+
+@poci_cmd(pattern="ii(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**MISKIN MISKIN AJA TOD, GAUSAH SOK SOK AN JADI DONATUR!!**")
+
+
+@poci_cmd(pattern="jj(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**GCAST DULU AHHHHH, KALI AJA MENANG GIPEAWAY!!**")
+
+
+@poci_cmd(pattern="kk(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**JAWABAN YANG BENER YANG MANA ANJENG**")
+
+
+@poci_cmd(pattern="ll(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**PAANSI SOALNYA PRIK BANGET!!**")
+
+
+@poci_cmd(pattern="mm(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**NAJISSS GIPEAWAY NYA CUMAN CEBAN YANG BANYAKAN NAPAH!!**")
+
+
+@poci_cmd(pattern="nn(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**OMEGAT GUA MENANG GIPEAWAY, MAYAN BUAT PICIES!!** 🥵🥵")
+
+
+@poci_cmd(pattern="oo(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**RAME JUGA DISINI, IKUTAN AHHH!!**")
+
+
+@poci_cmd(pattern="pp(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**GC ANJING SOALNYA GA DANTA SEMUA DAHLAH!!**")
+
+
+@poci_cmd(pattern="qq(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**HETTT SOALNYA GAMPANG BET, BARI MEREM GE BISA INI MAH** 🥱")
+
+
+@poci_cmd(pattern="rr(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**UDAH UDAH GIPEAWAY NYA UDAHAN, YAH KASIAN BET KETINGGALAN**")
+
+
+@poci_cmd(pattern="ss(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**CAPE CAPE GIKES SOALNYA GA DANTA KONTOLL**")
+
+
+@poci_cmd(pattern="tt(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**CLUE NYA APA ANJENG, SOALNYA GAJELAS**")
+
+    
+@poci_cmd(pattern="uu(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**DIMANA ADA GIPEAWAY DISITU ADA GUA WHAHAHAHAHA**")
+    
+   
+@poci_cmd(pattern="vv(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
+
+
+@poci_cmd(pattern="ww(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
+
+
+@poci_cmd(pattern="xx(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
+
+
+@poci_cmd(pattern="yy(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
+
+
+@poci_cmd(pattern="zz(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**GUA MAU NURUNIN SOAL NIH , BENTAR MIKIR DULU**")
+    
+
+CMD_HELP.update(
+    {
+        "gipeaway": f"**Plugin : **`edan`\
+        \n\nㅤㅤ•**Syntax** : {cmd}a-z\
+        \n•**Function : **Kata-Kata buat orang edan\
+    "
+    })
