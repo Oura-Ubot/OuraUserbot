252409081cb472ee6534d085427c8c1e8fa246e7
OURA? XINN? <99221379+Oura-Ubot@users.noreply.github.com>
2022-05-16 23:50:38 +0700

    Update wartel.py

--- userbot/modules/wartel.py
+++ userbot/modules/wartel.py
@@ -11,3 +11,12 @@ from userbot.utils import edit_or_reply, poci_cmd
 @poci_cmd(pattern="dekk(?: |$)(.*)")
 async def _(event):
     await edit_or_reply(event, "**`𝙄𝙉𝙂𝘼𝙏 𝘿𝙀𝙆 𝘾𝙄𝙉𝙏𝘼 𝙏𝘼𝙆 𝙎𝙀𝙇𝘼𝙈𝘼𝙉𝙔𝘼 𝙄𝙉𝘿𝘼𝙃`**")
+
+
+CMD_HELP.update(
+    {
+        "gipeaway": f"**Plugin : **`wartel`\
+        \n\nㅤㅤ•**Syntax** : {cmd}a-z\
+        \n•**Function : **Kata-Kata dari warga telegram\
+    "
+    })
