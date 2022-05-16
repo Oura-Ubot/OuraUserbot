95ec90393ebc1508977ce6a844d88e10e100f781
OURA? XINN? <99221379+Oura-Ubot@users.noreply.github.com>
2022-05-16 23:49:00 +0700

    Create wartel.py

---
+++ userbot/modules/wartel.py
@@ -0,0 +1,13 @@
+# @Owaitingforyou
+# Oura-Userbot
+# Recode by @OuraCakep
+
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import edit_or_reply, poci_cmd
+
+
+@poci_cmd(pattern="dekk(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(event, "**`𝙄𝙉𝙂𝘼𝙏 𝘿𝙀𝙆 𝘾𝙄𝙉𝙏𝘼 𝙏𝘼𝙆 𝙎𝙀𝙇𝘼𝙈𝘼𝙉𝙔𝘼 𝙄𝙉𝘿𝘼𝙃`**")
